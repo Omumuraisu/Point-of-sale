a68416054b2de0324e8f2e1a94f2a174f91778af
Omumuraisu <rycedoesknow@gmail.com>
2026-03-07 13:14:35 +0800

    g

---
+++ src/app/_layout.jsx
@@ -0,0 +1,20 @@
+import { Stack, Tabs } from 'expo-router';
+
+export default function Layout() {
+  return (
+
+    <Tabs>
+ <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+        }}
+      />    </Tabs>
+  );
+}
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Lyrics from '../components/Lyrics';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>tE SaN O siYA mArEPLy</Text>
       <StatusBar style="auto" />
+      <Lyrics />
     </View>
   );
 }
@@ -16,5 +18,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    height: '100%',
+    width: '100%',
   },
 });
